6875fedee3ece5c2883dfb337a4cd7ac2721b623
IdleWolf <3466503232@qq.com>
2026-01-31 21:15:46 +0800

    Update

---
+++ Assets/Scripts/Attachs/CharaSpriteAttach.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSpriteAttach : MonoBehaviour
+{
+    public Transform anchor;
+    public List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+
+    private void Awake()
+    {
+        if (spriteRenderers != null && spriteRenderers.Count > 0)
+        {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+                spriteRenderer.sortingLayerName = "Scene";
+        }
+
+    }
+    void Update()
+    {
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+            spriteRenderer.sortingOrder = (int)(200f + anchor.position.y);
+    }
+}
